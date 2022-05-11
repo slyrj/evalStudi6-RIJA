38b5fe4754b5558126bb02f936692d04180631ae
slyrj <slyrj@hotmail.fr>
2022-05-11 17:43:08 +0200

    maj et insertion html heads

---
+++ src/components/PrestHead.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const PrestHead = () => {
+  return (
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <link rel="shortcut icon" href="./media/favicon.ico" />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous"
+      />
+      <title>Chartles Cantin - Prestations</title>
+      <meta
+        name="description"
+        content="Page de présrations du site web de Charles Cantin - Photographe"
+      />
+    </head>
+  );
+};
+
+export default PrestHead;
