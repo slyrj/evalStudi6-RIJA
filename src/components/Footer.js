d83f14fb6b718d6b4ff3db6101c06a9b9c339b2f
slyrj <slyrj@hotmail.fr>
2022-05-11 20:35:32 +0200

    Insertion html et styles <footer>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="socialNetwork">
+      <ul>
+        <li>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+        </li>
+      </ul>
+
+      <span className="signature">
+        <p> © Tous droits réservés - Charles Cantin </p>
+      </span>
+      <span className="signature">
+        <NavLink exact to="/mentions-legales">
+          <p className="legacy">Mentions légales</p>
+        </NavLink>
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
