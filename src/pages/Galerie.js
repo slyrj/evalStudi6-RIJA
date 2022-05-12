b49dc51b936303889d6b8282669c6618ff9c6a09
slyrj <slyrj@hotmail.fr>
2022-05-12 19:06:58 +0200

    Insertion html et styles <galerie>

---
+++ src/pages/Galerie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GalerieHead from '../components/GalerieHead';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+
+const Galerie = () => {
+    return (
+        <>
+          <GalerieHead />
+           <Navigation />  
+        </>
+    );
+};
+
+export default Galerie;
