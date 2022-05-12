2da4ee87e3581432f5db7d04016fe15fc42220f0
slyrj <slyrj@hotmail.fr>
2022-05-12 15:44:27 +0200

    Création dossier data/Prestations.js + insertion données

---
+++ src/data/Prestations.js
@@ -0,0 +1,47 @@
+const  Prestations =[
+    {
+        id: 1,
+        titre: 'Juste moi',
+        prix: '130€',
+        text: 'Séance pour une personne, en extérieur ou en studio',
+        picture: './media/me.png'
+      },
+      {
+        id: 2,
+        titre: 'Pour deux',
+        prix: '195€',
+        text: 'Pour deux personnes, en extérieur ou en studio',
+        picture: './media/couple.png'
+      },
+      {
+        id: 3,
+       titre: 'Famille',
+       prix: '220€',
+       text: 'Pour la famille ou les amis jusqu’à 4 personnes, en extérieur ou en studio 30 euros en supplément par personne au-delà de 4 (hormis enfant jusqu’à 2 ans)',
+        picture: './media/familly.png'
+      },
+      {
+        id: 4,
+       titre: 'Il était une fois',
+       prix: '160€',
+       text: 'Photo de grossesse (À votre domicile, en extérieur ou en studio)',
+        picture: './media/once.png'
+      },
+      {
+        id: 5,
+       titre: 'Mon bébé',
+       prix: '100€',
+       text: 'Photo d’enfant jusqu’à 3 ans (photo à domicile)',
+        picture: './media/baby.png'
+      },
+      {
+        id: 6,
+       titre: 'J’immortalise l’événement',
+       prix: '120€',
+       text: 'Prestation de mariage ou baptême sur devis',
+        picture: './media/lego.png'
+      },
+    
+]
+    export default Prestations;
+    
