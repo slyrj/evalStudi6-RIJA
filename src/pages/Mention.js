4c87221c5c6b434184a0c9e8011ca8171ff8402e
slyrj <slyrj@hotmail.fr>
2022-05-12 18:13:42 +0200

    Insertion html et styles <mention> et <notFound>

---
+++ src/pages/Mention.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+import MentionHead from '../components/MentionHead';
+
+const Mention = () => {
+    return (
+        <>
+        <MentionHead />
+        <main>
+        <Navigation />
+      
+       <h3>Mentions légales</h3>
+            <p> 
+            Conditions Générales d'Utilisation
+Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par Carole J. Photographie et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ».
+
+ 
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+ 
+
+<h3> Conditions générales d’utilisation du site</h3> 
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+
+3. Services proposés
+Le site www.charlescantin.fred ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariaturf, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site www.carolejphotographie.com ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.
+
+​
+
+<h3> Collecte des données personnelles </h3>
+En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l’article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.
+
+A l’occasion de l’utilisation du site www.carolejphotographie.com, peuvent êtres recueillies : l’URL des liens par l’intermédiaire desquels l’utilisateur a accédé au site www.carolejphotographie.com, le fournisseur d’accès de l’utilisateur, l’adresse de protocole Internet (IP) de l’utilisateur.
+
+​
+
+En tout état de cause Carole Juin ne collecte des informations personnelles relatives à l’utilisateur que pour le besoin de certains services proposés par le site www.carolejphotographie.com, et notamment la demande de renseignement via le formulaire de contact de la page "PARLONS-EN". L’utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu’il procède par lui-même à leur saisie. Il est alors précisé à l’utilisateur du site www.carolejphotographie.com l’obligation ou non de fournir ces informations.
+
+ 
+
+Aucune information personnelle de l’utilisateur du site www.carolejphotographie.com n’est publiée à l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. 
+
+​
+
+En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, vous pouvez à tout moment demander un droit d'accès aux données vous concernant, de rectification, de suppression ou exercer votre droit à la limitation du traitement de vos données, en effectuant une demande écrite et signée, accompagnée d’une copie du titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée :
+
+​
+
+par mail à l'adresse mail : carolejphotographie@gmail.com
+
+​
+
+Consultez le site cnil.fr pour plus d’informations sur vos droits.
+
+​
+
+Si vous estimez, après nous avoir contactés, que vos droits « Informatique et Libertés » ne sont pas respectés, vous pouvez adresser une réclamation à la CNIL.
+
+​
+
+Le site n’est pas déclaré à la CNIL car il ne recueille pas d’informations personnelles. .
+
+​
+
+Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
+
+​
+
+5. Propriété intellectuelle et contrefaçons
+Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.
+
+L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.
+
+Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.
+
+Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.
+
+ 
+
+6. Limitations de responsabilité
+Les sources des informations diffusées sur le site www.carolejphotographie.com sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.
+
+Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site www.carolejphotographie.com ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.
+
+Le site www.carolejphotographie.com ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.
+
+La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
+
+ 
+
+7. Liens hypertextes
+Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qn’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.
+
+ 
+
+8. Cookies
+L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.
+
+Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’UtiliLes cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.
+
+L’information contenue dans les co
+
+L’Utilisateur doit toutefois donner son consentement quant à l’utilisation de certains cookies.
+
+A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.
+
+L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.
+
+ 
+9. Droit applicable et attribution de juridiction
+La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.
+
+​
+
+Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.</p>
+
+<Footer />
+        </main>
+     
+        </>
+    );
+};
+
+export default Mention;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import NotFoundHead from '../components/NotFoundHead';
+const NotFound = () => {
+  return (
+      <>
+    <NotFoundHead />
+    <div className='notFound'>
+        <div className='notFoundContent'>
+          <h3>Désolé, cette page n'existe pas !</h3>
+          <NavLink exact to="/"> 
+          <i className="fas fa-home"></i>
+          <span> Accueil</span>
+          </NavLink>
+        </div>
+    </div>
+    </>
+  ) 
+};
+export default NotFound;
